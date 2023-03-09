15d0ea0c3487343c48c49114eb45a8cfaf2100ae
tomer <tomerraviv95@gmail.com>
2023-03-09 09:38:27 +0200

    Initial commit of evaluation working. Only plotter has to be changed

--- python_code/plotters/plotter_main.py
+++ python_code/plotters/plotter_main.py
@@ -4,7 +4,7 @@ from python_code.plotters.plotter_utils import plot_by_values
 
 ## Plotter for the Paper's Figures
 if __name__ == '__main__':
-    run_over = False  # whether to run over previous results
+    run_over = True  # whether to run over previous results
     trial_num = 1  # number of trials per point estimate, used to reduce noise by averaging results of multiple runs
     plot_type = PlotType.COMPARE_BLACK_BOX_AND_MODEL_BASED  # Choose the plot among the three Figures
     print(plot_type.name)
