2bc78ee28c58e3f49b57e35569d0f6ccee09ab4b
tomer <tomerraviv95@gmail.com>
2023-03-24 15:47:52 +0300

    Updated the configs for the plotter, and added the config for the SISO case

--- python_code/plotters/plotter_config.py
+++ python_code/plotters/plotter_config.py
@@ -6,6 +6,7 @@ from python_code.utils.constants import DetectorType
 
 class PlotType(Enum):
     LINEAR_SYNTH_QPSK = 'LINEAR_SYNTH_QPSK'
+    LINEAR_SYNTH_QPSK_SISO = 'LINEAR_SYNTH_QPSK_SISO'
 
 
 def get_config(plot_type: PlotType) -> Tuple[List[Dict], list, str, str]:
@@ -59,6 +60,60 @@ def get_config(plot_type: PlotType) -> Tuple[List[Dict], list, str, str]:
             {'snr': 15, 'detector_type': DetectorType.meta_deepsic.name,
              'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
             {'snr': 16, 'detector_type': DetectorType.meta_deepsic.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+        ]
+        values = list(range(10, 17, 1))
+        xlabel, ylabel = 'SNR [dB]', 'BER'
+    elif plot_type == PlotType.LINEAR_SYNTH_QPSK_SISO:
+        params_dicts = [
+            {'snr': 10, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 11, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 12, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 13, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 14, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 15, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 16, 'detector_type': DetectorType.joint_rnn.name},
+            {'snr': 10, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 11, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 12, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 13, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 14, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 15, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 16, 'detector_type': DetectorType.online_rnn.name},
+            {'snr': 10, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 11, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 12, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 13, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 14, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 15, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 16, 'detector_type': DetectorType.joint_viterbinet.name},
+            {'snr': 10, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 11, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 12, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 13, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 14, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 15, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 16, 'detector_type': DetectorType.online_viterbinet.name},
+            {'snr': 10, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 11, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 12, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 13, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 14, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 15, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 16, 'detector_type': DetectorType.meta_viterbinet.name},
+            {'snr': 10, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 11, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 12, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 13, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 14, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 15, 'detector_type': DetectorType.meta_viterbinet.name,
+             'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
+            {'snr': 16, 'detector_type': DetectorType.meta_viterbinet.name,
              'aug_type': ['geometric_augmenter', 'translation_augmenter', 'rotation_augmenter'], 'online_repeats_n': 3},
         ]
         values = list(range(10, 17, 1))
